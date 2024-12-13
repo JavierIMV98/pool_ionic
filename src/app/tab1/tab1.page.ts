21e8b02dcc8e0bdf710a6f565aed216a632bf1d1
Javier Moya <moyavalerio.ji@gmail.com>
2024-12-12 21:45:23 -0300

    cargar desde bd tab 1 (pendiente todas las validaciones e insertar desde el modal idealmente...)

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonGrid, IonRow, IonCol, IonButton, IonModal } from '@ionic/angular/standalone';
 import { CardMesaComponent } from '../card-mesa/card-mesa.component';
-
+import { Mesa } from '../services/database.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgFor } from '@angular/common';
 import { ModalController } from '@ionic/angular/standalone';
 import { ModalCrearMesaComponent } from '../modal-crear-mesa/modal-crear-mesa.component';
+import { DatabaseService } from '../services/database.service';
 
 @Component({
   selector: 'app-tab1',
@@ -16,19 +17,20 @@ import { ModalCrearMesaComponent } from '../modal-crear-mesa/modal-crear-mesa.co
   providers: [ModalController]
 })
 export class Tab1Page {
+  mesas = this.database.getMesas(); // Recupera las mesas desde la base de datos
   private currentModal: HTMLIonModalElement | null = null;
   message : string = '';
 
 
 
-  items = [
+  /* items = [
     { imagen: 'assets/poolball1.png', inicio: '14:00', precio: '90' },
     { imagen: 'assets/poolball2.png', inicio: '15:30', precio: '120' },
     { imagen: 'assets/poolball3.png', inicio: '16:45', precio: '75' },
-  ];
-  constructor(private router: Router, private route: ActivatedRoute, private modalController: ModalController) {}
-  goToDetails(item: any) {
-    this.router.navigate(['detalles-mesa'], { relativeTo: this.route, queryParams: item });
+  ]; */
+  constructor(private router: Router, private route: ActivatedRoute, private modalController: ModalController, private database: DatabaseService) {}
+  goToDetails(mesa: Mesa) {
+    this.router.navigate(['detalles-mesa'], { relativeTo: this.route, queryParams: mesa });
   }
   async openModalCrearMesa(){
     this.currentModal = await this.modalController.create({
